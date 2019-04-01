a9f5be3625a3c8c0c3a7f8a2cf7534c12148885d
Talia Jeffrey <tajeffrey11@gmail.com>
2019-04-01 22:09:22 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -4,7 +4,29 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+        var term = $("input").val();
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q="+term+"&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response){
+           var image = response.data[0].images.original.url;
+            $("#pic").append("<img src=" + image + ">");
+           
+        response.data.forEach(function(arrayElement) {
+            var image = arrayElement.images.original.url;
+            $("#pic").append(`<a href = "${image}">
+            <img src= "${image} ">
+            </a>
+            `);
+        }); 
+
+            
+        }
+            
+        
+        
+            
+        });
   
   
 });
